72876ca6381d0d3e877129ff9e472232248fbbe7
Gina Son <ginasunah33@gmail.com>
2017-07-14 16:06:08 -0400

    part4 completed

---
+++ contact.rb
@@ -0,0 +1,7 @@
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note, :submit
+
+  def initialize(contact)
+
+  end
+end
--- crm.rb
+++ crm.rb
@@ -9,3 +9,31 @@ end
 get '/contact' do
   erb :index
 end
+
+get '/contact/new' do
+
+end
+
+get 'contact/:id' do
+
+end
+
+get '/contacts/:id/edit' do
+
+  @contact = contact.find_by(id: params[:id].to_i)
+    if @contact
+    erb :edit_contact
+    else
+    raise Sinatra::NotFound
+    end
+end
+
+post '/contact' do
+  Contact.create(
+  first_name: params[:first_name],
+  last_name:  params[:last_name],
+  email:      params[:email],
+  note:       params[:note]
+  )
+  redirect to('/contact')
+end
